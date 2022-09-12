86871dafbac61787f4d4deeffef44f19714d2f58
reky <me@reky.dev>
2022-09-12 12:31:58 +0700

    Fix arithmetic operation

--- arithmetic.py
+++ arithmetic.py
@@ -15,12 +15,12 @@ def divide(bilangan_pertama: int or float, bilangan_kedua: int or float) -> int 
 
 
 def mod(bilangan_pertama: int or float, bilangan_kedua: int or float) -> int or float:
-    return bilangan_pertama / bilangan_kedua
+    return bilangan_pertama % bilangan_kedua
 
 
 def exponent(bilangan_pertama: int or float, bilangan_kedua: int or float) -> int or float:
-    return bilangan_pertama / bilangan_kedua
+    return bilangan_pertama ** bilangan_kedua
 
 
 def floor_divide(bilangan_pertama: int or float, bilangan_kedua: int or float) -> int or float:
-    return bilangan_pertama / bilangan_kedua
+    return bilangan_pertama // bilangan_kedua
