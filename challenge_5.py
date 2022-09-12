6d57f3eb3d988535fcdaea23ae6fea7dcb058e1e
reky <me@reky.dev>
2022-09-12 14:48:18 +0700

    Add infinite looping

--- challenge_5.py
+++ challenge_5.py
@@ -1,32 +1,35 @@
 from all_challenge import *
 
-option = select_menu()
+while True:
+    clear_screen()
+    option = select_menu()
 
-match option:
-    case "1":
-        challenge_1()
+    match option:
+        case "1":
+            challenge_1()
 
-    case "2":
-        challenge_2()
+        case "2":
+            challenge_2()
 
-    case "3":
-        challenge_3()
+        case "3":
+            challenge_3()
 
-    case "4":
-        challenge_4()
+        case "4":
+            challenge_4()
 
-    case "5":
-        modulus()
+        case "5":
+            modulus()
 
-    case "6":
-        exponentiation()
+        case "6":
+            exponentiation()
 
-    case "7":
-        floor_division()
+        case "7":
+            floor_division()
 
-    case "8":
-        exit("Keluar dari program")
+        case "8":
+            print("Keluar dari program")
+            break;
 
-    case _:
-        print("""
-    Nomer yang anda input tidak ada pada menu""")
+        case _:
+            print("""
+        Nomer yang anda input tidak ada pada menu""")
